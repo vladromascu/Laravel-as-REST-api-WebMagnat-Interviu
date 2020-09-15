d82b8bd4e69c7bd8c4b2d398cab362ae35c01078
unknown <vladdrema9809@yahoo.com>
2020-09-16 01:41:07 +0300

    creare data de baze si citire records

---
+++ app/Models/CountryModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountryModel extends Model
+{
+    protected $table='_z_country';
+
+    protected $fillable=[
+        'iso',
+        'name',
+        'dname',
+        'iso3',
+        'postition',
+        'numcode',
+        'phonecode',
+        'created',
+        'register_by',
+        'modified',
+        'modified_by',
+    ];
+}
